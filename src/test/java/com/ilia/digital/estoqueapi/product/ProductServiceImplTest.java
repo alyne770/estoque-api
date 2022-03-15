8ab54022bebe7f4f05959b558076d9cad5e39e4f
Luiz Nicolau Lemos Freitas <luiz.freitas@ilia.digital.com>
2022-03-15 13:45:27 -0300

    Metrics with Prometheus

--- src/test/java/com/ilia/digital/estoqueapi/product/ProductServiceImplTest.java
+++ src/test/java/com/ilia/digital/estoqueapi/product/ProductServiceImplTest.java
@@ -4,10 +4,13 @@ import com.ilia.digital.estoqueapi.MockUtil;
 
 import com.ilia.digital.estoqueapi.domain.Product;
 
+import com.ilia.digital.estoqueapi.dto.CreateProductDto;
+import com.ilia.digital.estoqueapi.dto.UpdateProductDto;
 import com.ilia.digital.estoqueapi.exception.BadRequestException;
 import com.ilia.digital.estoqueapi.repository.ProductRepository;
 import com.ilia.digital.estoqueapi.service.product.impl.ProductServiceImpl;
 
+import com.ilia.digital.estoqueapi.util.PageableUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import org.assertj.core.api.Assertions;
@@ -18,10 +21,14 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
-
+import java.util.List;
 import java.util.Optional;
 
 
@@ -32,7 +39,8 @@ class ProductServiceImplTest {
     ProductServiceImpl productServiceImpl;
     @Mock
     ProductRepository productRepository;
-
+    @Mock
+    ModelMapper mapper;
     @Test
     void findById_cannotReturnNullOrDifferentProductIfProductExists() {
 
@@ -57,14 +65,39 @@ class ProductServiceImplTest {
 
     @Test
     void findAByNameOrCode() {
+        BDDMockito.when(productRepository.findAByNameOrCode(ArgumentMatchers.any(Pageable.class),
+                ArgumentMatchers.anyString())).thenReturn(new PageImpl(List.of(MockUtil.getProductWithId())));
+
+        Page<Product> page = productServiceImpl.findAByNameOrCode(PageableUtil.configuringPageable(null, null,
+                null, null), "");
+
+        Assertions.assertThat(page).isNotEmpty();
+        Assertions.assertThat(page.toList()).isNotEmpty();
+        Assertions.assertThat(page.toList().size()).isEqualTo(1);
+        Assertions.assertThat(page.toList().get(0)).isEqualTo(MockUtil.getProductWithId());
     }
 
     @Test
     void create() {
+        BDDMockito.when(productRepository.save(ArgumentMatchers.any(Product.class))).thenReturn(MockUtil.getProductWithId());
+
+        CreateProductDto createProduct = mapper.map(MockUtil.getProductWithOutId(),CreateProductDto.class);
+        Product product = productServiceImpl.create(createProduct);
+
+        Assertions.assertThat(product).isNotNull().isEqualTo(MockUtil.getProductWithId());
     }
 
     @Test
     void replace() {
+        Product substituteProduct =  MockUtil.getProductWithId();
+        substituteProduct.setName("Name of Product 2");
+        substituteProduct.setCode("HB-110000");
+
+        BDDMockito.when(productRepository.save(ArgumentMatchers.any(Product.class))).thenReturn(substituteProduct);
+
+        Product product = productServiceImpl.replace(new UpdateProductDto());
+
+        Assertions.assertThat(product).isNotNull().isEqualTo(substituteProduct);
     }
 
 
